e25fef055c0b13d8bafd0d02de051747a2f0daa5
Jasper Park <jasper.park@bluehands.de>
2023-10-30 15:34:44 +0100

    feat: half-revert changes to get functioning version

---
+++ src/contracts/index.ts
@@ -0,0 +1,5 @@
+export * from './http-client';
+export * from './http-error-response';
+export * from './http-headers';
+export * from './http-headers-factory';
+export * from './http-response';
--- src/operators/navigate.ts
+++ src/operators/navigate.ts
@@ -2,7 +2,7 @@ import { Observable, OperatorFunction, concatMap, of } from "rxjs";
 import { Result, Failure, isSuccess } from 'fnxt/result'
 import { ProblemDetailsError } from "../problem-details-error";
 
-export function navigate<T, R>(project: (value: T, index: number) => Observable<Result<R, ProblemDetailsError>>): OperatorFunction<Result<T, ProblemDetailsError>, Result<R, ProblemDetailsError>> {
+export const navigate = <T, R>(project: (value: T, index: number) => Observable<Result<R, ProblemDetailsError>>): OperatorFunction<Result<T, ProblemDetailsError>, Result<R, ProblemDetailsError>> => {
     return concatMap((result: Result<T, ProblemDetailsError>, index: number): Observable<Result<R, ProblemDetailsError>> => {
         if (isSuccess(result)) {
             return project(result.value, index);
